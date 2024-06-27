eab06eb565b692274f3829d79c68af0a42212c46
lvis04 <lvis@tuta.io>
2024-06-27 12:23:53 -0300

    commit 1.1, aprendendo ternarias, e deixando o código mais enxuto.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,57 +3,36 @@ const form = document.querySelector('#form');
 function recebeEventoForm(e){
     e.preventDefault(); 
 
-    const inputPeso = form.querySelector('#peso')
-    const inputAltura = form.querySelector('#altura')
-    const peso = parseFloat(inputPeso.value)
-    const altura = parseFloat(inputAltura.value)
+    const peso = form.querySelector('#peso').value
+    const altura = form.querySelector('#altura').value
     const imc = getIMC(peso, altura)
 
     const resultadoDiv = document.querySelector('#resultado');
     resultadoDiv.innerHTML = '';
 
-
-    if (!peso){
-        resultado.innerHTML = "peso inválido";
-        return;
+    const verificarCampos = (peso, altura) => {
+        return !peso ? (resultado.innerHTML = "peso inválido", true)
+                     : !altura ? (resultado.innerHTML = "Altura iválida", true)
+                     : false;
     }
-    if (!altura){
-        resultado.innerHTML = "Altura inválida";
-        return;
-    }
+    if (verificarCampos(peso, altura)) return;    
 
+    const imcMensagem = imc => {
 
-    if (imc < 18.5){
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está abaixo do peso ideal!`
-
-    }
-    else if (imc >= 18.5 && imc <= 24.9){
-
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está no peso ideal!`
-    }
-    else if (imc >= 25 && imc <= 29.9){
-
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está com sobrepeso!`
-    }
-    else if (imc >= 30 && imc <= 34.9){
-
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está com Obesidade Grau I`
-    }
-    else if (imc >= 35 && imc <= 39.9){
-    
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está com Obesidade Grau II`
-    }
-    else{
-    
-        resultado.innerHTML += `Seu IMC é de ${imc}. Você está com Obesidade Grau III`
+        if (imc < 18.5) return `IMC: ${imc}; Você está abaixo do peso ideal`
+        if (imc >= 18.5 && imc <= 24.9) return `IMC: ${imc}; Você está no peso ideal`
+        if (imc >= 25 && imc <= 29.9) return `IMC: ${imc}; Você está com sobrepeso`
+        if (imc >= 30 && imc <= 34.9) return `IMC: ${imc}; Você está com Obesidade Grau I`
+        if (imc >= 35 && imc <= 29.9) return `IMC: ${imc}; Você está com Obesidade Grau II`
+        return `IMC: ${imc}; Você está com Obesidade Grau III`
     }
 
-
+    resultado.innerHTML += imcMensagem(imc);
 }
+
 function getIMC(peso, altura){
     const imc = peso / altura ** 2;
     return imc.toFixed(2);    
 }
-
 
 form.addEventListener('submit', recebeEventoForm)
